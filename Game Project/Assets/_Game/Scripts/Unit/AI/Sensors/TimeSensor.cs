2485eccb1d06456237ed8e165e6f6bc636defa5d
Andres Urquijo <andyurquijo@gmail.com>
2016-03-13 23:55:32 -0400

    Build Fixes

--- Game Project/Assets/_Game/Scripts/Unit/AI/Sensors/TimeSensor.cs
+++ Game Project/Assets/_Game/Scripts/Unit/AI/Sensors/TimeSensor.cs
@@ -1,35 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+#if UNITY_EDITOR
 using UnityEditor;
-
+#endif
 // Sensor used to determine passage of time for an AI Agent. Time can be slowed down or sped up.
-public class TimeSensor : MonoBehaviour {
+public class TimeSensor : MonoBehaviour
+{
 
-	public float timer;
-	public float Value
-	{
-		get { return timer; }
-	}
+    public float timer;
+    public float Value
+    {
+        get { return timer; }
+    }
 
-	private float modifier;
-	public float Modifier
-	{
-		get { return modifier; }
-		set { modifier = value; }
-	}
+    private float modifier;
+    public float Modifier
+    {
+        get { return modifier; }
+        set { modifier = value; }
+    }
 
-	void Update () {
-		timer += Time.deltaTime*modifier;
-	}
+    void Update()
+    {
+        timer += Time.deltaTime * modifier;
+    }
 
-	public void Reset()
-	{
-		timer = 0;
-	}
-
+    public void Reset()
+    {
+        timer = 0;
+    }
+#if UNITY_EDITOR
     public void OnDrawGizmos()
     {
-        if(timer != 0)
-            Handles.Label(transform.position + Vector3.down*0.5f, "Timer: " + timer);
+        if (timer != 0)
+            Handles.Label(transform.position + Vector3.down * 0.5f, "Timer: " + timer);
     }
+#endif
 }
